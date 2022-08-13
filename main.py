28d0bbf3695261f634b73a103681a5d7786b8e90
Christian Antony Quero <christianantonyquero@gmail.com>
2022-08-14 05:58:41 +0800

    Add CLI

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from canvas import Canvas
 from shapes import Square, Rectangle
 
-canvas = Canvas(20, 30, (255, 255, 255))
+canvas_width = int(input("Enter canvas width:"))
+canvas_height = int(input("Enter canvas height:"))
 
-r1 = Rectangle(1, 6, 7, 10, (100, 200, 125))
-r1.draw(canvas)
+colors = {"black": (255, 255, 255), "white": (0, 0, 0)}
+canvas_color = input("Enter canvas color (white or black): ")
 
-s1 = Square(1, 3, 3, (0, 100, 222))
-s1.draw(canvas)
-canvas.make("canvas.png")
+canvas = Canvas(height=canvas_height, width=canvas_width, color=colors[canvas_color])
+
+while True:
+    shape_type = input("What do you like to draw? Enter quit to quit. ")
+
+    if shape_type.lower() == "rectangle":
+        rec_x = int(input("Enter x of the rectangle: "))
+        rec_y = int(input("Enter y of the rectangle: "))
+        rec_w = int(input("Enter the width of the rectangle: "))
+        rec_h = int(input("Enter the height of the rectangle: "))
+        red = int(input("How much red should the rectangle have? "))
+        green = int(input("How much green? "))
+        blue = int(input("How much blue? "))
+        r1 = Rectangle(x=rec_x, y=rec_y,height=rec_h, width=rec_w,color=(red, green, blue))
+        r1.draw(canvas)
+
+    if shape_type.lower() == "square":
+        sqr_x = int(input("Enter the x of the square: "))
+        sqr_y = int(input("Enter the y of the square: "))
+        sqr_side = int(input("Enter the side of the square: "))
+        red = int(input("How much red should the rectangle have? "))
+        green = int(input("How much green? "))
+        blue = int(input("How much blue? "))
+        s1 = Square(x=sqr_x, y=sqr_y, side=sqr_side, color=(red, green, blue))
+        s1.draw(canvas)
+    if shape_type.lower() == "quit":
+        break
+canvas.make('canvas.png')
